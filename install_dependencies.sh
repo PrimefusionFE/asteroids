03e53a66c8088345e00ff65ca25720d59bfa51ee
PrimefusionFE <primeabyss@yahoo.com>
2025-08-12 20:26:58 -0700

    Update dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 cd ~/dev/bootdev
 uv init asteroids
 cd asteroids
-un venv
+uv venv
 source .venv/bin/activate
 uv add pygame==2.6.1
-un run main.py
+uv run main.py

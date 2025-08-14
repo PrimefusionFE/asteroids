53303ab4f5b216884c47667c3b7161decb17c910
PrimefusionFE <primeabyss@yahoo.com>
2025-08-13 17:54:09 -0700

    Add delta time to game loop

--- main.py
+++ main.py
@@ -2,38 +2,34 @@ import pygame
 from pygame.locals import *
 from constants import *
 
-MS_PER_UPDATE = 60
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    num_pass, num_fail = pygame.init()
+    pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_loop(screen)
 
 def game_loop(screen):
-    previous_time = pygame.time.get_ticks()
-    lag = 0.0
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        current_time = pygame.time.get_ticks()
-        elapsed_time = current_time - previous_time
-        previous_time= current_time
-        lag += elapsed_time
-    
         handle_input(screen)
 
-        while lag >= MS_PER_UPDATE:
-            update(screen)
-            lag -= MS_PER_UPDATE
+        update(screen)
 
         render(screen)
+
+        dt = (clock.tick(fps) / 1000)
 
 def handle_input(screen):
     pass

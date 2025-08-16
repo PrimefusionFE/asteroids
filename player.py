ea9c691aea8e8d6c9e240b00fcf485c6c79db81e
PrimefusionFE <primeabyss@yahoo.com>
2025-08-16 15:20:39 -0700

    Add ability to shoot

--- player.py
+++ player.py
@@ -1,23 +1,12 @@
 import pygame
 from circleshape import CircleShape
+from shot import Shot
 from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
-    # in the player class
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
-    
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -33,6 +22,12 @@ class Player(CircleShape):
         
         if keys[pygame.K_s]:
             self.move(dt * -1)
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)            
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -40,4 +35,18 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1)
+        shot.velocity = shot.velocity.rotate(self.rotation) 
+        shot.velocity *= PLAYER_SHOOT_SPEED 
+        print(shot.velocity)      
     
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]  
